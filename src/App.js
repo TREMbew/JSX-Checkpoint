6c44ad1994bc70f0f8e99d317001795baee751cb
Mohamed Mhamdi <mhamdi.mohamed944@gmail.com>
2020-12-07 01:37:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import naruto from './imageInSrc.jpg';
+import myVideo from './myVideo.mp4'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={{border:'solid 1px black', maxWidth:'100vw'}}>
+      <h1 className="title red">Your Name Here</h1>
+      <br></br>
+      <img src={naruto} alt="naruto"/>
+      <br></br>
+      <img src="/imageInPublic.png" alt="kakashi"/>
     </div>
+
+    <video style={{width:320, height:240}} controls>
+      <source src={myVideo} type="video/mp4" />
+    </video>
+    </>
   );
 }
 
